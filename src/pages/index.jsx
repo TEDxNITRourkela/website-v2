5f9b22fc71d31b2dc91d1a75bae73eb098d31c61
Ritesh Patil <riteshsp2000@gmail.com>
2021-11-02 18:02:23 +0530

    feat: initialise firebase sdk

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
 
-const Homepage = () => (
-  <div>
-    <h2>Hello</h2>
-  </div>
-);
+// Libraries
+import { getFirestore, onSnapshot, doc } from 'firebase/firestore';
+
+// Utils
+import getFirebase from '../utils/firebase';
+
+const app = getFirebase();
+let db;
+if (app) db = getFirestore(app);
+
+const Homepage = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const getTrialData = async () => {
+      // eslint-disable-next-line no-unused-vars
+      const unsub = onSnapshot(doc(db, 'trial', 'trialDoc'), (snap) => {
+        setData(snap.data());
+        return Promise.resolve();
+      });
+    };
+
+    const fetchData = async () => {
+      await Promise.all([getTrialData()]);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <h2>{data?.text}</h2>
+      <img src={data?.img} alt='Trial' />
+    </div>
+  );
+};
 
 export default Homepage;
