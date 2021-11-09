b9ae7bfc006a4fef9bfc8befe04a9c57035df83a
Ritesh Patil <riteshsp2000@gmail.com>
2021-11-09 09:08:56 +0530

    feat: create primary typography components in components/shared/typography directory

---
+++ src/components/shared/typography/Heading3.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+/**
+ * Used in the teams component for team headings.
+ */
+export default styled.h3`
+  font-family: Zilla Slab;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 28px;
+  line-height: 1.6;
+  text-align: center;
+  color: rgba(256, 256, 256, 0.4);
+`;
---
+++ src/components/shared/typography/Para2.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+/**
+ * Mainly used for Navbar and Footer content
+ */
+export default styled.p`
+  font-family: Helvetica;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 1.6;
+  color: #ffffff;
+`;
