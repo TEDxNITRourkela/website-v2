c67b425d23e6067901e0b96a704f55bf922c5e16
Sambit Sankalp <sambit.sankalp.official@gmail.com>
2021-11-10 23:09:02 +0530

    style:responsiveness updated

--- src/components/shared/index.js
+++ src/components/shared/index.js
@@ -2,3 +2,10 @@
 
 // Marginal Layout
 export { default as Layout } from './Layout';
+
+export { default as Heading1 } from './typography/Heading1';
+export { default as Heading2 } from './typography/Heading2';
+export { default as Heading3 } from './typography/Heading3';
+export { default as Para1 } from './typography/Para1';
+export { default as Para2 } from './typography/Para2';
+export { default as Digitised } from './typography/DigitisedHeading';
