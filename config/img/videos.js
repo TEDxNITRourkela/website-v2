4ff0436a5bf4f29b2138e969170a2d0aa8aa6b7d
Sonakshi1901 <sonakshi1901@gmail.com>
2021-11-23 00:03:03 +0400

    feat: created the 404 page

---
+++ config/img/videos.js
@@ -0,0 +1,11 @@
+/* eslint-disable max-len */
+export const VIDEOS = Object.freeze({
+    SCROLL_DOWN:
+      'https://res.cloudinary.com/tedxnitrourkela/video/upload/assets/gif/scrollDown_gj8lj3.mp4',
+    PEAK:
+      'https://res.cloudinary.com/tedxnitrourkela/video/upload/assets/gif/peak_wzuwvd.mp4',
+    LOADING:
+      'https://res.cloudinary.com/tedxnitrourkela/video/upload/assets/gif/loading_zqfkiz.mp4',
+    PAGE_NOT_FOUND:
+      'https://res.cloudinary.com/tedxnitrourkela/video/upload/assets/gif/animation_500_klesmt8f_ufu16j.mp4',
+  });
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,11 +1,149 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// Components
+// Libraries
+import styled from 'styled-components';
+import { CustomButton } from '../components';
 
-const Error404 = () => (
-  <div>
-    <h1 style={{ width: '100%', textAlign: 'center', paddingTop: '10rem' }}>404 Page Not Found</h1>
-  </div>
-);
+// Assets
+import { LOGOS } from '../../config/img/logos';
+import { GRAPHICS } from "../../config/img/graphics";
+import { VIDEOS } from '../../config/img/videos';
 
-export default Error404;
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: calc(100vh - 150px);
+  height: 100%;
+`;
+
+const Section = styled.div`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Content = styled.h2`
+  color: var(--color-secondary);
+  opacity: 0.2;
+  font-family: 'Zilla Slab';
+  letter-spacing: 3;
+
+  @media (max-width: 540px){
+    font-size: 0.75rem;
+  }
+`;
+
+const Logo = styled.img`
+  margin-top: 20px;
+  margin-bottom: 30px;
+  width: 30%;
+
+  @media (max-width: 540px){
+    width: 80%;
+  }
+`;
+
+const Topic = styled.h1`
+  margin-top: 10px;
+  margin-bottom: 20px;
+  font-weight: 500;
+  color: var(--color-secondary);
+  font-family: 'Zilla Slab';
+`;
+
+const ComingSoon = styled.video`
+  width: 220px;
+  height: 220px;
+  margin-top: 40px;
+
+  @media (max-width: 540px){
+    width: 170px;
+    height: 170px;
+  }
+`;
+
+const Error = styled.h2`
+  margin-top: 20px;
+  margin-bottom: 20px;
+  color: var(--color-secondary);
+  font-family: 'Zilla Slab';
+`;
+
+const TopRight = styled.img`
+  position: absolute;
+  top: 0px;
+  right: 0px;
+
+  @media (max-width: 540px){
+    width: 30%;
+    height: auto;
+  }
+`;
+
+const BottomLeft = styled.img`
+  position: absolute;
+  top: 100px;
+  left: 0px;
+
+  @media (max-width: 540px){
+    width: 30%;
+    height: auto;
+  }
+`;
+
+function Index() {
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = () => {
+    if (loading === false) {
+      setLoading(true);
+    }
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  };
+
+  return (
+    <Wrapper>
+      <Section>
+        <Content>
+          TECHNOLOGY . ENTERTAINMENT . DESIGN
+        </Content>
+        <Logo
+          src={LOGOS.TEDxNITRourkela.LIGHT}
+          alt='Logo'
+        />
+        <Topic>
+          &#8220;Learning from the Past&#8221;
+        </Topic>
+
+        <ComingSoon>
+          <source src={VIDEOS.PAGE_NOT_FOUND} type='video/mp4' />
+        </ComingSoon>
+        <Error>
+          Error: Page not found!
+        </Error>
+
+        <CustomButton loading={loading} setLoading={setLoading} handleClick={handleClick}>
+          Go Home
+        </CustomButton>
+      </Section>
+
+      <TopRight
+        src={GRAPHICS.TOP_RIGHT}
+        alt='ellipse'
+      />
+      <BottomLeft
+        src={GRAPHICS.BOTTOM_LEFT}
+        alt='ellipse'
+      />
+    </Wrapper>
+  );
+}
+
+export default Index;
