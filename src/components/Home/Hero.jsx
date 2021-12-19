aeaad5993c40c585c19268794a135782b452eff5
Ritesh Patil <riteshsp2000@gmail.com>
2021-12-19 19:26:19 +0530

    fix: build errors 2

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -13,7 +13,12 @@ import calculateTimeLeft, { getlength } from '../../utils/timer';
 
 const Hero = () => {
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-  const { caption, logo, heading } = home;
+  const { caption, heading } = home;
+
+  const logo = {
+    src: 'https://res.cloudinary.com/sambitsankalp/image/upload/v1636208790/TedxNITR/White_Logo_ldguyq.png',
+    alt: 'TedxNITRourkela',
+  };
 
   useEffect(() => {
     setTimeout(() => {
