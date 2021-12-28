79a3cad954913abbf4418d0c13d8cbc7d0bb6a63
Ritesh Patil <riteshsp2000@gmail.com>
2021-12-28 23:27:55 +0530

    feat: setup all-mdx and generate event pages using gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,38 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require('path');
 
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  // Create blog pages
+  const { createPage } = actions;
+  const postTemplate = path.resolve(`src/components/templates/Event.jsx`);
 
+  const result = await graphql(`
+    {
+      allMdx(filter: { fileAbsolutePath: { regex: "/content/live/" } }) {
+        edges {
+          node {
+            frontmatter {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`);
+    return;
+  }
+
+  // Create post detail pages
+  const events = result.data.allMdx.edges;
+
+  events.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.slug,
+      component: postTemplate,
+      context: {},
+    });
+  });
+};
