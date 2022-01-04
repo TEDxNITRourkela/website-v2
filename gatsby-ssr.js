05df9367f089ccb9ce159dcacd5db046a2bca873
Ritesh Patil <riteshsp2000@gmail.com>
2022-01-04 22:57:33 +0530

    feat: fetch data from firebase

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,12 @@
-import React from 'react';
-
 // Components
+import * as React from 'react';
+import loadable from '@loadable/component';
 import { App } from './src/components';
 
-export const wrapRootElement = ({ element }) => <App>{element}</App>;
+const FirebaseProvider = loadable(() => import('./src/utils/firebase'));
+
+export const wrapRootElement = ({ element }) => (
+  <FirebaseProvider>
+    <App>{element}</App>
+  </FirebaseProvider>
+);
